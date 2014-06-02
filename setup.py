d44d609cf13b604de721ad3e1f9ee9e34fde2657
Matthew King <automatthew@gmail.com>
2014-06-02 13:20:28 -0500

    initial setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='bitvault',
+      version='0.1.0',
+      description='Python client for BitVault.io',
+      url='http://github.com/BitVault/bitvault-py',
+      author='Dustin Laurence',
+      author_email='dustin@pandastrike.com',
+      license='MIT',
+      packages=['bitvault'],
+      install_requires=[
+      ],
+      zip_safe=False)
+
