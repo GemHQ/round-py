62483b94ed6b800ab1032b8ac764ff83108acfc0
Dustin Laurence <dustin@laurences.net>
2014-06-25 09:32:23 -0700

    Format cleanup

--- bitvault/__init__.py
+++ bitvault/__init__.py
@@ -1,51 +1,64 @@
+# patchboard.py
+#
+# Copyright 2014 BitVault.
+
+
 import base64
 
 import patchboard
 
 from .client import Client
 
-api_url = "http://bitvault.pandastrike.com/"
+
+api_url = u"http://bitvault.pandastrike.com/"
+
 
 __patchboard_client = None
+
 
 def client(url=api_url):
     global __patchboard_client
 
     if __patchboard_client is None:
-        __patchboard_client = patchboard.discover(url, {'default_context': Context})
+        __patchboard_client = patchboard.discover(url,
+                                                  {u'default_context': Context})
     return Client(__patchboard_client.spawn())
 
+
 def authed_client(url=api_url,
-        email=None, password=None,
-        app_url=None, api_token=None):
+                  email=None,
+                  password=None,
+                  app_url=None,
+                  api_token=None):
     cl = client(url)
     if email and password:
         cl.context.set_basic(email, password)
     elif app_url and api_token:
         cl.context.set_token(api_token)
     else:
-        raise ValueError("Must supply either email and password, or url and token")
+        raise ValueError(
+            u"Must supply either email and password, or url and token")
     return cl
+
 
 class Context(dict):
 
     def authorizer(self, scheme, resource, action):
-        if scheme == "Basic":
-            if hasattr(self, 'basic'):
+        if scheme == u"Basic":
+            if hasattr(self, u'basic'):
                 return self.basic
             else:
-                raise Exception("Must call set_basic(email, password) first")
-            
-        elif scheme == "BitVault-Token":
-            if hasattr(self, 'api_token'):
+                raise Exception(u"Must call set_basic(email, password) first")
+
+        elif scheme == u"BitVault-Token":
+            if hasattr(self, u'api_token'):
                 return self.api_token
             else:
-                raise Exception("Must call set_token(api_token) first")
+                raise Exception(u"Must call set_token(api_token) first")
 
     def set_basic(self, email, password):
-        string = ':'.join([email, password])
+        string = u':'.join([email, password])
         self.basic = base64.b64encode(string)
 
     def set_token(self, token):
         self.api_token = token
-
--- bitvault/client.py
+++ bitvault/client.py
@@ -1,5 +1,6 @@
 from bitvault import collections
 
+
 class Client(object):
 
     def __init__(self, client):
@@ -7,6 +8,3 @@ class Client(object):
         self.context = self.client.context
         self.resources = self.client.resources
         self.users = collections.Users(resource=self.resources.users)
-
-
-
