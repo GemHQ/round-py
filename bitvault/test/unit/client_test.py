b5c5624ff95c3ae47ca7883841b62811963a6bc9
Dustin Laurence <dustin@laurences.net>
2014-06-25 17:58:06 -0700

    Test for exact type

--- bitvault/test/unit/client_test.py
+++ bitvault/test/unit/client_test.py
@@ -28,46 +28,46 @@ class TestResourceCreation:
 
     def test_users(self, users):
         assert users
-        assert isinstance(users, bitvault.collections.Users)
+        assert type(users) == bitvault.collections.Users
 
     def test_user(self, user):
         assert user
-        assert isinstance(user, bitvault.wrappers.User)
+        assert type(user) == bitvault.wrappers.User
 
     def test_apps(self, apps):
         assert apps
-        assert isinstance(apps, bitvault.collections.Applications)
+        assert type(apps) == bitvault.collections.Applications
 
     def test_app(self, app):
         assert app
-        assert isinstance(app, bitvault.wrappers.Application)
+        assert type(app) == bitvault.wrappers.Application
 
     def test_wallets(self, wallets):
         assert wallets
-        assert isinstance(wallets, bitvault.collections.Wallets)
+        assert type(wallets) == bitvault.collections.Wallets
 
     def test_locked_wallet(self, locked_wallet):
         assert locked_wallet
         assert locked_wallet.is_locked()
-        assert isinstance(locked_wallet, bitvault.wrappers.Wallet)
+        assert type(locked_wallet) == bitvault.wrappers.Wallet
 
     def test_wallet(self, wallet):
         assert wallet
         assert wallet.is_unlocked()
-        assert isinstance(wallet, bitvault.wrappers.Wallet)
+        assert type(wallet) == bitvault.wrappers.Wallet
 
     def test_accounts(self, accounts):
         assert accounts
-        assert isinstance(accounts, bitvault.collections.Accounts)
+        assert type(accounts) == bitvault.collections.Accounts
 
     def test_account(self, account):
         assert account
-        assert isinstance(account, bitvault.wrappers.Account)
+        assert type(account) == bitvault.wrappers.Account
 
     def test_addresses(self, addresses):
         assert addresses
-        assert isinstance(addresses, patchboard.resources.Addresses)
+        assert type(addresses) == patchboard.resources.Addresses
 
     def test_address(self, address):
         assert address
-        assert isinstance(address, patchboard.util.SchemaStruct)
+        assert type(address) == patchboard.util.SchemaStruct
