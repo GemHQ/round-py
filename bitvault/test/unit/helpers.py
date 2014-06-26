c1cf8e7104f565621fa4ad3f8f89ad151c04c919
Dustin Laurence <dustin@laurences.net>
2014-06-26 06:59:35 -0700

    Add missing file

---
+++ bitvault/test/unit/helpers.py
@@ -0,0 +1,56 @@
+# unit/helpers.py
+#
+# Copyright 2014 BitVault.
+#
+# Broken out of fixtures.py so that in the future they can be used in the
+# tests as well.
+
+
+import time
+from random import randint
+
+
+def bitvault_url():
+    return u"http://localhost:8998"
+
+
+def current_milli_time():
+    return int(round(time.time()))
+
+
+def email():
+    addr = u'{0}@bitvault.io'.format(randint(0, 2**32 - 1))
+    print("Address:", addr)
+    return addr
+
+
+def password():
+    return u'incredibly_secure'
+
+
+def app_name():
+    return u'bitcoins_r_us'
+
+
+def locked_wallet_name():
+    return u'my locked wallet'
+
+
+def wallet_name():
+    return u'my favorite wallet'
+
+
+def locked_wallet_passphrase():
+    return u"a very bad passphrase"
+
+
+def wallet_passphrase():
+    return u"wrong pony generator brad"
+
+
+def callback_url():
+    return u'https://someapp.com/callback'
+
+
+def account_name():
+    return u'office supplies'
