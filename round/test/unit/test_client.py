28265b4e3e18596040bf6d4105838cbdd9df9f40
Matt Smith <matt.the.smith@gmail.com>
2014-12-08 18:43:29 -0800

    added client.developer test

--- round/test/unit/test_client.py
+++ round/test/unit/test_client.py
@@ -129,92 +129,9 @@ class TestClient:
         with raises(ValueError):
             client.authenticate_device(app.api_token, "key", "secret")
 
+    def test_developer(self, client, developer):
+        with raises(round.AuthenticationError):
+            client.developer
 
-
-    # def test_developers(self, developers):
-    #     assert developers
-    #     assert type(developers) == round.developers.Developers
-
-    # def test_developer(self, developer):
-    #     assert developer
-    #     assert type(developer) == round.developers.Developer
-
-    # def test_developer_update(self, developer):
-    #     new_email = email()
-    #     developer = developer.update(email=new_email)
-    #     assert developer
-    #     assert type(developer) == round.developers.Developer
-    #     assert developer.email == new_email
-
-    # def test_apps(self, apps):
-    #     assert type(apps) == round.dict_wrappers.Applications
-
-    # def test_app(self, app):
-    #     assert app
-    #     assert type(app) == round.wrappers.Application
-
-    # def test_app_update(self, app):
-    #     app = app.update(name="wrenches")
-    #     assert app
-    #     assert type(app) == round.wrappers.Application
-    #     assert app.name == "wrenches"
-
-    # def test_users(self, users):
-    #     assert type(users) == round.dict_wrappers.Users
-
-    # def test_user(self, user):
-    #     assert(user)
-    #     assert type(user) == round.wrappers.User
-
-    # def test_user_update(self, user):
-    #     new_name = {"first_name": "John", "last_name": "Johnson"}
-    #     user = user.update(**new_name)
-    #     assert user
-    #     assert type(user) == round.wrappers.User
-    #     for (key, value) in new_name.items():
-    #         assert user[key] == value
-
-    # def test_wallets(self, wallets):
-    #     assert type(wallets) == round.dict_wrappers.Wallets
-
-    # def test_wallet(self, wallet):
-    #     assert wallet
-    #     assert wallet.is_unlocked()
-    #     assert type(wallet) == round.wrappers.Wallet
-
-    # def test_wallet_update(self, wallet):
-    #     wallet = wallet.update(name="support")
-    #     assert wallet
-    #     assert type(wallet) == round.wrappers.Wallet
-    #     assert wallet.name == "support"
-
-    # def test_accounts(self, accounts):
-    #     assert type(accounts) == round.dict_wrappers.Accounts
-
-    # def test_account(self, account):
-    #     assert account
-    #     assert type(account) == round.wrappers.Account
-
-    # def test_account_update(self, account):
-    #     account = account.update(name="staples")
-    #     assert account
-    #     assert type(account) == round.wrappers.Account
-    #     assert account.name == "staples"
-
-
-    # def test_addresses(self, addresses):
-    #     assert addresses is not None
-    #     assert type(addresses) == round.list_wrappers.Addresses
-
-    # def test_address(self, address):
-    #     assert address is not None
-    #     assert type(address) == patchboard.util.SchemaStruct
-
-    # def test_basic_auth(self, developer, app):
-    #     auth = dict(email=developer.email, password=password())
-    #     client = round.authenticate(developer=auth)
-    #     developer = client.developer
-    #     assert developer
-    #     for name, application in developer.applications.iteritems():
-    #         assert application
-    #         assert type(app) == round.wrappers.Application
+        client.authenticate_developer(email=developer.email, privkey=privkey())
+        assert client.developer.email == developer.email
