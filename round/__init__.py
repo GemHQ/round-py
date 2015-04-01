a00cca6082f62971503b0d189b7d4649267fbdf6
Matt Smith <matt.the.smith@gmail.com>
2015-04-01 00:48:29 -0700

    docstringz

--- round/__init__.py
+++ round/__init__.py
@@ -3,12 +3,7 @@
 #
 # Copyright 2014 BitVault, Inc. dba Gem
 
-import base64
-import json
 import patchboard
-
-from pprint import pprint as pp
-from time import time
 
 # TODO: PSS when ruby can handle it.
 from Crypto.PublicKey import RSA
@@ -28,87 +23,100 @@ def client(url=DEFAULT_URL, network=DEFAULT_NETWORK):
         _patchboard = {}
 
     if url not in _patchboard:
-        _patchboard[url] = patchboard.discover(url,
-                                                  {u'default_context': Context})
+        _patchboard[url] = patchboard.discover(
+            url, {u'default_context': Context})
     return Client(_patchboard[url].spawn(), network)
 
 
 class Context(dict):
+    """Context holds parameters for authenticating to Gem under various schemes.
+    This class allows for multiple concurrent authentication levels. It can only
+    hold one instance of any parameter, so new Contexts should be created to
+    store multiple instances of the same scheme.
+
+    __init__ takes no arguments.
+
+    Attributes:
+      schemes (dict of dict): A collection whose keys are authentication schemes
+        supported by Gem and whose values are dicts containing parameters and
+        credentials for that scheme.
+    """
 
     def __init__(self):
         self.schemes = {
-            u'Gem-Developer-Session':
-                {u'usage':
-                     u"DEPRECATE",
-                 u'params': [u'key', u'session_token']},
-            u'Gem-User-Session':
-                {u'usage':
-                     u"DEPRECATE",
-                 u'params': [u'key', u'session_token']},
             u'Gem-Application':
-                {u'usage':
-                     u"client.authenticate_application(app_url=app_url, api_token=token, instance_id=instance_id [, override=False, fetch=True])",
-                 u'params': [u'app_url', u'api_token', u'instance_id']},
+                 {u'params': [u'app_url', u'api_token', u'instance_id']},
             u'Gem-Device':
-                {u'usage':
-                     u"client.authenticate_device(api_token=token, user_token=token, device_id=device_id [, email=user_email, user_url=user_url, override=False, fetch=True])",
-                 u'params': [u'api_token', u'user_email', u'user_url', u'user_token', u'device_id']},
+                 {u'params': [u'api_token', u'user_email', u'user_url',
+                              u'user_token', u'device_id']},
             u'Gem-Identify':
-                {u'usage':
-                     u"client.authenticate_identify(api_token=token [, override=False])",
-                 u'params': [u'api_token']},
+                 {u'params': [u'api_token']},
             u'Gem-MFA':
-                {u'usage':
-                     u"client.authenticate_mfa(auth_token=token [, mfa_token=token])",
-                 u'params': [u'auth_token']},
+                {u'params': [u'auth_token']},
             u'Gem-Password':
-                {u'usage':
-                     u"client.authenticate_password(auth_token=token [, mfa_token=token])",
-                 u'params': [u'password_hash']}
+                {u'params': [u'password_hash']}
         }
 
     def authorizer(self, schemes, resource, action, request_args):
+        """Construct the Authorization header for a request.
+
+        Args:
+          schemes (list of str): Authentication schemes supported for the
+            requested action.
+          resource (str): Object upon which an action is being performed.
+          action (str): Action being performed.
+          request_args (list of str): Arguments passed to the action call.
+
+        Returns:
+          (str, str) A tuple of the auth scheme satisfied, and the credential
+            for the Authorization header or empty strings if none could be
+            satisfied.
+        """
         if not schemes:
             return u'', u''
         for scheme in schemes:
             if scheme in self.schemes and u'credential' in self.schemes[scheme]:
-                creds = self.schemes[scheme][u'credential']
+                cred = self.schemes[scheme][u'credential']
                 if hasattr(self, 'mfa_token'):
-                    creds = '{}, mfa_token="{}"'.format(creds, self.mfa_token)
-                return scheme, creds
-
+                    cred = '{}, mfa_token="{}"'.format(creds, self.mfa_token)
+                return scheme, cred
 
         raise AuthenticationError(self, schemes)
 
     def authorize(self, scheme, **params):
+        """Store credentials required to satisfy a given auth scheme.
+
+        Args:
+          scheme (str): The name of the Authentication scheme.
+          **params: parameters for the specified scheme.
+
+        Returns:
+          The Authorization header string generated for the scheme.
+        """
         if scheme not in self.schemes:
             return False
 
+        # TODO: Do we store params on the scheme when they were provided or
+        # (as is the current case) globally, accessible to all schmes?
         for field in self.schemes[scheme]['params']:
             if field in params and params[field]:
                 setattr(self, field, params[field])
-            if field in [u'privkey', u'app_url', u'user_url', u'user_email']:
+            if field in [u'app_url', u'user_url', u'user_email']:
                 del params[field]
 
         self.schemes[scheme][u'credential'] = Context.format_auth_params(params)
         return self.schemes[scheme][u'credential']
 
-
-    def dev_signature(self, request_body):
-        try:
-            body = json.loads(request_body) if request_body else {}
-            key = RSA.importKey(self.privkey)
-            signer = PKCS1_v1_5.new(key)
-            ts = int(time())
-            content = u'{}-{}'.format(json.dumps(body, separators=(',',':')), ts)
-            digest = SHA256.new(content)
-            return base64.urlsafe_b64encode(signer.sign(digest)), ts
-        except Exception as e:
-            pp(e)
-            raise Exception(u"You must provide a valid RSA private key.")
-
     @staticmethod
     def format_auth_params(params):
+        """Generate the format expected by HTTP Headers from parameters.
+
+        Args:
+          params (dict): {key: value} to convert to key=value
+
+        Returns:
+          A formatted header string.
+        """
         parts = []
         for (key, value) in params.items():
             parts.append('{}="{}"'.format(key, value))
--- round/subscriptions.py
+++ round/subscriptions.py
@@ -8,14 +8,16 @@ from .config import *
 from .wrappers import *
 
 class Subscriptions(ListWrapper):
+    """A collection of Subscription objects"""
 
     def create(self, callback_url):
-        """
-        Create a new Subscription on all addresses contained by this collection's
-        parent object.
-        Return the new round.Subscription object.
-        Keyword arguments:
-        callback_url -- URI of an active endpoint which can receive notifications
+        """Register a new Subscription on this collection's parent object.
+
+        Args:
+          callback_url: URI of an active endpoint which can receive notifications
+
+        Returns:
+           A round.Subscription object if successful.
         """
         resource = self.resource.create({u'subscribed_to': u'address',
                                          u'callback_url': callback_url})
@@ -24,19 +26,33 @@ class Subscriptions(ListWrapper):
         return subscription
 
     def add(self, subscription):
+        """Add a Subscription to this collection.
+
+        Args:
+          subscription (round.Subscription)
+        """
         self.data.append(subscription)
 
     def wrap(self, resource):
+        """Construct a Subscription object from a server-side resource.
+
+        Args:
+          resource (patchboard.Resource)
+        """
         return Subscription(resource, self.client)
 
 
 class Subscription(Wrapper, Updatable):
+    """A Subscription represents interest in any events on bitcoin addresses
+    related to the Subscription's parent object.
+
+    __init__ wraps an existing patchboard object that correlates to a server-side
+    Subscription.
+
+    Args:
+      resource (patchboard.Resource)
+      client (round.Client)
+    """
 
     def __init__(self, resource, client):
-        """
-        Initialize a round.Subscription from an Account patchboard.Resource object.
-        Keyword arguments:
-        resource --  Subscription patchboard.Resource object
-        client -- authenticated round.Client object
-        """
         super(Subscription, self).__init__(resource, client)
